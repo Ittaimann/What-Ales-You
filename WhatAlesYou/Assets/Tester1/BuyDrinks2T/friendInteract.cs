5777b330cdbc3418634e226eb327c9cbfb9cc5ce
seantsukim <seantsukim@gmail.com>
2018-05-19 19:01:51 -0700

    OOF
    very buggy but working on it

---
+++ WhatAlesYou/Assets/Tester1/BuyDrinks2T/friendInteract.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cradle;
+
+public class friendInteract : MonoBehaviour {
+	public Story story1;
+	public Story story2;
+	public GameObject twinePlayer1;
+	public GameObject twinePlayer2;
+	public GameObject background1;
+	public GameObject background2;
+
+	public StoryOutput cout;
+
+	public bool customerInteract = false; //if we initiated contact
+	public bool waitingDrink = false; //if they ordered drink and wait for player
+	public bool isPaused = false;
+
+	public bool passageOneDone = false;
+	public bool passageTwoRun = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	//when u click on friend, this object will pop up the dialogue.
+	//if friend is talking, the dialogue box will pop up once and 
+	//twine will take over.
+	void OnMouseDown()
+	{
+		//set bool to true to allow only one click to start dialogue.
+		if (customerInteract == false)
+		{
+			customerInteract = true;
+
+			//code block that pops up dialogue and runs twine dialogue.
+			twinePlayer1.GetComponent<TwineTextPlayer>().AutoDisplay = true;
+			twinePlayer1.GetComponent<TwineTextPlayer>().StartStory = true;
+			background1.GetComponent<RectTransform>().localScale = new Vector3(0.8f, background1.GetComponent<RectTransform>().localScale.y, background1.GetComponent<RectTransform>().localScale.z);
+
+			if (twinePlayer1.GetComponent<TwineTextPlayer>().AutoDisplay)
+			{
+				story1.Begin();
+			}
+		}
+
+		if (waitingDrink)
+		{
+			isPaused = false;
+			waitingDrink = false;
+		}
+
+	}
+
+/*
+   void enterOrder()
+   {
+       isPaused = true;
+
+       //pauses the dialogue, and removes UI elements to allow for drink mixing gameplay
+       twinePlayer1.GetComponent<TwineTextPlayer>().AutoDisplay = false;
+       twinePlayer1.GetComponent<TwineTextPlayer>().StartStory = false;
+       twinePlayer1.GetComponent<RectTransform>().localScale = new Vector3(0.0f, background1.GetComponent<RectTransform>().localScale.y, background1.GetComponent<RectTransform>().localScale.z);
+
+       waitingDrink = true;
+   }
+
+   void giveDrink()
+   {
+       isPaused = false;
+
+       //resumes game when player gives customer the drink
+       twinePlayer1.GetComponent<TwineTextPlayer>().AutoDisplay = true;
+       twinePlayer1.GetComponent<TwineTextPlayer>().StartStory = true;
+       twinePlayer1.GetComponent<RectTransform>().localScale = new Vector3(0.8f, background1.GetComponent<RectTransform>().localScale.y, background1.GetComponent<RectTransform>().localScale.z);
+
+   }
+*/
+
+    // Update is called once per frame 
+    void Update () {
+		if (story1.CurrentPassage.Name == "Order Up" && Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			passageOneDone = true;
+			Debug.Log("Hello");
+			
+		}
+
+		if (passageOneDone)
+		{
+			passageTwoRun = true;
+			twinePlayer1.GetComponent<TwineTextPlayer>().AutoDisplay = false;
+       		twinePlayer1.GetComponent<TwineTextPlayer>().StartStory = false;
+       		background1.GetComponent<RectTransform>().localScale = new Vector3(0.0f, background1.GetComponent<RectTransform>().localScale.y, background1.GetComponent<RectTransform>().localScale.z);
+			passageOneDone = false;
+
+			twinePlayer2.GetComponent<TwineTextPlayer>().AutoDisplay = true;
+       		twinePlayer2.GetComponent<TwineTextPlayer>().StartStory = true;
+       		background2.GetComponent<RectTransform>().localScale = new Vector3(0.8f, background2.GetComponent<RectTransform>().localScale.y, background2.GetComponent<RectTransform>().localScale.z);
+			story2.Begin();
+		}
+	}
+}
---
+++ WhatAlesYou/Assets/Tester1/GoodDay/TesterzCues.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cradle;
+using UnityEngine.UI;
+
+public class TesterzCues : MonoBehaviour {
+
+	public Story story;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	[StoryCue("They Good", "Enter")]
+	void enterGood()
+	{
+		//story.GetComponent<Image>().color = Color.yellow;
+	}
+
+	[StoryCue("They Bad", "Enter")]
+	void enterBad()
+	{
+		//story.GetComponent<Image>().color = Color.blue;
+	}
+
+	[StoryCue("Ending", "Enter")]
+	void enterEnd()
+	{
+		//story.GetComponent<Image>().color = Color.black;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
