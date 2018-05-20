828256ddcf3772c74139ca17aa3419a620cae9e6
Wayne <weiyanz@uci.edu>
2018-05-20 13:04:40 -0700

    Modify setting of steve + animation testing

---
+++ WhatAlesYou/Assets/scripts/AnimationManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AnimationManager : MonoBehaviour {
+	
+	public void Start()
+	{
+		SitDown();
+	}
+
+	public void SitDown()
+	{
+		GetComponent<Animator>().SetBool("IsSitting", true);
+	}
+
+}
